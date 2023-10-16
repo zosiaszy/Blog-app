5e69472a7d681f273ad9a82fb8d44b03fe499a8e
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-10-16 20:56:13 +0200

    Add PostAdd

--- src/components/pages/PostAdd.js
+++ src/components/pages/PostAdd.js
@@ -1,14 +1,27 @@
 
 import React from "react";
+import AddPostForm from "./AddPostForm";
 
 const PostAdd = () => {
 
     return (
 
         <div> 
-        
-            <h2>PostAdd</h2>
+            
+           <div className="container">
+    <div className="row justify-content-center">
+   
 
+    <div className="col-md-8 mt-3">
+
+        <h2> Post ADD </h2>
+    
+    </div>
+ </div>
+ </div>
+
+
+            <AddPostForm />
 
         </div>   
 
