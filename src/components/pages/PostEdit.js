44cb7189c1c0bab1312dcb4ad665dcac658b62f7
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-10-17 12:47:44 +0200

    Render EditForm

--- src/components/pages/PostEdit.js
+++ src/components/pages/PostEdit.js
@@ -1,24 +1,13 @@
-
 import React from "react";
+import EditPostForm from '../features/EditPostForm';
+
 const PostEdit = () => {
-
     return (
-
-        <div> 
-        
-            <h2>PostEdit</h2>
-
-
-        </div>   
-
-
-
-
+        <div>
+            <h2>Edit Post</h2>
+            <EditPostForm />
+        </div>
     );
-
-
-
 }
-
 
 export default PostEdit;
