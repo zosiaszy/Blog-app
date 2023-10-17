dabc6c08a957ef5004a40846a5bdabce516185e7
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-10-17 12:45:57 +0200

    Add EDIT_POST

--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -6,6 +6,7 @@ import shortid from 'shortid';
 const createActionName = actionName => `app/posts/${actionName}`;
 const REMOVE_POST = createActionName('REMOVE_POST');
 const ADD_POST = createActionName('ADD_POST');
+const EDIT_POST = createActionName('EDIT_POST');
 // action creators
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
@@ -16,6 +17,9 @@ const postsReducer = (statePart = [], action) => {
 
     case ADD_POST:
       return [...statePart, { ...action.payload, id: shortid() }];
+
+    case EDIT_POST:
+     return statePart.map(post => (post.id === action.payload.id ? { ...post, ...action.payload } : post));
 
     default:
       return statePart;
@@ -30,8 +34,14 @@ export const removePost = (postId) => ({
 export const addPost = (post) => ({
   type: ADD_POST,
   payload: post,
+});
 
 
-})
+export const editPost = (post) => ({
+  type: EDIT_POST,
+  payload: post,
+
+
+});
 
 export default postsReducer;
