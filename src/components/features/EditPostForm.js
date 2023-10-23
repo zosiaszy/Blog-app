c37212d6db2b796a31998e6f99a1471ffa8d8df8
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-10-23 10:12:55 +0200

    Add changes EditPostForm

--- src/components/features/EditPostForm.js
+++ src/components/features/EditPostForm.js
@@ -5,40 +5,37 @@ import { useNavigate, useParams } from 'react-router-dom';
 import PostForm from './PostForm';
 
 const EditPostForm = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const { id } = useParams(); 
-  const posts = useSelector(state => state.posts); 
-  const postToEdit = posts.find(post => post.id === id); 
+  const { id } = useParams();
+  const posts = useSelector(state => state.posts);
+  const postToEdit = posts.find(post => post.id === id);
 
-
-const handleSubmit = post => {
-  dispatch(editPost({ id, ...post }));
-  navigate('/');
-};
-
-
-
+  const handleSubmit = post => {
+    dispatch(editPost({ id, ...post }));
+    navigate('/');
+  };
 
   if (!postToEdit) {
-   
     navigate('/');
     return null;
   }
 
-  return (
-  <PostForm
-    action={handleSubmit}
-    actionText="Save Changes"
-    title={postToEdit.title} 
-    author={postToEdit.author}
-    publishedDate={postToEdit.publishedDate}
-    shortDescription={postToEdit.shortDescription}
-    content={postToEdit.content}
-  />
-);
+  
+  const formattedDate = postToEdit.publishedDate.toISOString();
 
+  return (
+    <PostForm
+      action={handleSubmit}
+      actionText="Save Changes"
+      title={postToEdit.title}
+      author={postToEdit.author}
+      publishedDate={formattedDate}
+      shortDescription={postToEdit.shortDescription}
+      content={postToEdit.content}
+    />
+  );
 };
 
 export default EditPostForm;
