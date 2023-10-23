611ea0ef6061eb89e20093a93cd32ca9edb21186
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-10-23 10:07:49 +0200

    Add dateToStr utils

---
+++ src/utils/dateToStr.js
@@ -0,0 +1,13 @@
+const formatDate = (date) => {
+  if (!(date instanceof Date) || isNaN(date.getTime())) {
+    return 'Invalid Date';
+  }
+
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+
+  return `${month}/${day}/${year}`;
+};
+
+export {formatDate};
