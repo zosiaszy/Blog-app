50cd7408c3bde544b45fc08aec9b95ef100cd1f0
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-10-23 10:13:43 +0200

    Add changes date

--- src/components/pages/PostSummary.js
+++ src/components/pages/PostSummary.js
@@ -2,25 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 
-
 const PostSummary = ({ title, shortDescription, content, author, publishedDate, id }) => {
- 
+  
+  const formattedDate = new Date(publishedDate).toLocaleDateString();
 
-  
-  
+
+
+  const formattedContent = { __html: content };
+
   return (
-    <div className="card mb-3 mt-3"> 
+    <div className="card mb-3 mt-3">
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
         <p className="card-text font-weight-bold">Author: {author}</p>
-        <p className="card-text font-weight-bold">Published Date: {publishedDate}</p>
+        <p className="card-text font-weight-bold">Published Date: {formattedDate}</p>
         <p className="card-text">{shortDescription}</p>
-        <p className="card-text">{content}</p>
-       
-          <Link to={`/post/${id}`}>
-            <Button variant="primary">Read more</Button>
-          </Link>
-        
+        <p className="card-text" dangerouslySetInnerHTML={formattedContent} />
+
+        <Link to={`/post/${id}`}>
+          <Button variant="primary">Read more</Button>
+        </Link>
       </div>
     </div>
   );
