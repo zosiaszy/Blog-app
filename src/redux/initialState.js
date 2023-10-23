397b31c9213bb28b78b9de4c2c1c291f277f46e8
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-10-23 10:13:16 +0200

    Add changes dates initialState

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,56 +1,38 @@
-
 const initialState = {
-
-    posts: [
-        
-        {
-            id: '1',
-            title: 'Article title',
-            shortDescription: 'Short description of the article...',
-            content: 'Main content of the article',
-            publishedDate: '02-02-2022',
-            author: 'John Doe'
-        },
-
-        {
-            id: '2',
-            title: 'Article nr 2',
-            shortDescription: 'Description of the article',
-            content: 'Main content of the article',
-            publishedDate: '04-05-2023',
-            author: 'John Doe'
-        },
-
-        {
-            id: '3',
-            title: 'Article nr 3',
-            shortDescription: 'Short description of the article...',
-            content: 'Main content of the article',
-            publishedDate: '05-06-2023',
-            author: 'John Doe'
-        },
-
-        {
-            id: '4',
-            title: 'Article nr 4',
-            shortDescription: 'Short description of the article...',
-            content: 'Main content of the article',
-            publishedDate: '02-07-2023',
-            author: 'John Doe'
-        }
-        
-
-
-
-
-    ]
-
-
-
-
-
-
-}
+  posts: [
+    {
+      id: '1',
+      title: 'Article title',
+      shortDescription: 'Short description of the article...',
+      content: 'Main content of the article',
+      publishedDate: new Date('02-02-2022'), // Zmiana formatu daty na obiekt Date
+      author: 'John Doe'
+    },
+    {
+      id: '2',
+      title: 'Article nr 2',
+      shortDescription: 'Description of the article',
+      content: 'Main content of the article',
+      publishedDate: new Date('04-05-2023'), // Zmiana formatu daty na obiekt Date
+      author: 'John Doe'
+    },
+    {
+      id: '3',
+      title: 'Article nr 3',
+      shortDescription: 'Short description of the article...',
+      content: 'Main content of the article',
+      publishedDate: new Date('05-06-2023'), // Zmiana formatu daty na obiekt Date
+      author: 'John Doe'
+    },
+    {
+      id: '4',
+      title: 'Article nr 4',
+      shortDescription: 'Short description of the article...',
+      content: 'Main content of the article',
+      publishedDate: new Date('02-07-2023'), // Zmiana formatu daty na obiekt Date
+      author: 'John Doe'
+    }
+  ]
+};
 
 export default initialState;
-
