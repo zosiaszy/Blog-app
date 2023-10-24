868d4ff973e5a01bd16b0d0d1b6b9a878a9ef0d4
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-10-24 21:16:35 +0200

    Add PostSummary

--- src/components/pages/PostSummary.js
+++ src/components/pages/PostSummary.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 
-const PostSummary = ({ title, shortDescription, content, author, publishedDate, id }) => {
+const PostSummary = ({ title, shortDescription, content, author, publishedDate, id}) => {
   
   const formattedDate = new Date(publishedDate).toLocaleDateString();
 
 
 
-  const formattedContent = { __html: content };
+ 
 
   return (
     <div className="card mb-3 mt-3">
@@ -17,7 +17,7 @@ const PostSummary = ({ title, shortDescription, content, author, publishedDate, 
         <p className="card-text font-weight-bold">Author: {author}</p>
         <p className="card-text font-weight-bold">Published Date: {formattedDate}</p>
         <p className="card-text">{shortDescription}</p>
-        <p className="card-text" dangerouslySetInnerHTML={formattedContent} />
+        <p className="card-text" dangerouslySetInnerHTML={{ __html: content }} />
 
         <Link to={`/post/${id}`}>
           <Button variant="primary">Read more</Button>
