b86bc075ab56b9c2c923cc48aad161fbe495a940
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-10-25 16:56:30 +0200

    Add categories initialState

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -6,32 +6,40 @@ const initialState = {
       shortDescription: 'Short description of the article...',
       content: 'Main content of the article',
       publishedDate: new Date('02-02-2022'),
-      author: 'John Doe'
+      author: 'John Doe',
+      category: 'Fun',
     },
     {
       id: '2',
       title: 'Article nr 2',
       shortDescription: 'Description of the article',
       content: 'Main content of the article',
-      publishedDate: new Date('04-05-2023'), 
-      author: 'John Doe'
+      publishedDate: new Date('04-05-2023'),
+      author: 'John Doe',
+      category: 'Lifestyle',
     },
     {
       id: '3',
       title: 'Article nr 3',
       shortDescription: 'Short description of the article...',
       content: 'Main content of the article',
-      publishedDate: new Date('05-06-2023'), 
+      publishedDate: new Date('05-06-2023'),
+      author: 'John Doe',
+      category: 'Mystery',
     },
     {
       id: '4',
       title: 'Article nr 4',
       shortDescription: 'Short description of the article...',
       content: 'Main content of the article',
-      publishedDate: new Date('02-07-2023'), 
-      author: 'John Doe'
-    }
-  ]
+      publishedDate: new Date('02-07-2023'),
+      author: 'John Doe',
+      category: 'Music',
+    },
+
+     
+  ],
+  categories: ['Fun', 'Sport', 'Extra', 'Lifestyle', 'Music', 'Mystery'],
 };
 
 export default initialState;
